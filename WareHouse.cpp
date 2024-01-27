529dcda054679a8e7f1b92bcfae161f826e1ee3c
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-01-27 11:54:00 +0200

    Update WareHouse.cpp

--- WareHouse.cpp
+++ WareHouse.cpp
@@ -18,8 +18,8 @@ WareHouse::WareHouse(const string &configFilePath){ //TODO intialize the rest of
     WareHouse::customerCounter = 1; //TODO remember started with 1. first assign customer, then increment by 1
     WareHouse::volunteerCounter = 1;
     WareHouse::orderCounter = 1;
-    WareHouse::volunteers = this->parseVolunteers(configFilePath);
-    WareHouse::customers = this->parseCustomers(configFilePath);
+    this->parseVolunteers(configFilePath);
+    this->parseCustomers(configFilePath);
     WareHouse::flagCustomer = new SoldierCustomer(-1,"def",-1,-1);
     WareHouse::flagVolunteer = new DriverVolunteer(-1,"def",-1,-1);
     WareHouse::flagOrder = new Order(-1,-1,-1);
@@ -48,35 +48,40 @@ bool WareHouse::newOrderbyID(int customerId){
 }
 
 void WareHouse::AddCustomer(string customerName, string customerType, int distance, int maxOrders){
-    if(customerType == "soldier"){
+    cout << WareHouse::customerCounter << endl;
+    if(customerType == "soldier" || customerType == "Soldier"){
         SoldierCustomer *soldierCust = new SoldierCustomer(WareHouse::customerCounter, customerName, distance, maxOrders);
         customers.push_back(soldierCust);
+        cout << soldierCust->toString() << endl;
+        cout << "soldier" << endl;
         WareHouse::customerCounter++;
     }
 
-    else if(customerType == "civilian"){
+    else if(customerType == "civilian" || customerType == "Civilian"){
         CivilianCustomer *civilianCust = new CivilianCustomer(WareHouse::customerCounter, customerName, distance, maxOrders);
         customers.push_back(civilianCust);
+        cout << civilianCust->toString() << endl;
+        cout << "civilian" << endl;
         WareHouse::customerCounter++;
     }
+    cout << WareHouse::customerCounter << endl;
+
 }
 
+void WareHouse::addAction(BaseAction* action){
+    WareHouse::actionsLog.push_back(action);
+}
 
-//TODO addaction
-
-//
-
-Customer &WareHouse::getCustomer(int customerId) const{ //only called if the customer exists. 
+Customer &WareHouse::getCustomer(int customerId) const{
     for (const auto& customer : WareHouse::customers) {
         if(customer->getId() == customerId){
             return (*customer);
         }
     }
     return *(WareHouse::flagCustomer); //avoids warnings
-
 }
 
-Volunteer &WareHouse::getVolunteer(int volunteerId) const{ //only called if the volunteer exists. 
+Volunteer &WareHouse::getVolunteer(int volunteerId) const{
     for (const auto& volunteer : WareHouse::volunteers) {
         if(volunteer->getId() == volunteerId){
             return (*volunteer);
@@ -85,7 +90,7 @@ Volunteer &WareHouse::getVolunteer(int volunteerId) const{ //only called if the 
     return *(WareHouse::flagVolunteer); //avoids warnings
 }
 
-bool WareHouse::volunteerExists(int volunteerId){ //TODO keep? maybe not used at all
+bool WareHouse::volunteerExists(int volunteerId){
     for (const auto& volunteers : volunteers) {
         if(volunteers->getId() == volunteerId){
             return true;
@@ -94,16 +99,11 @@ bool WareHouse::volunteerExists(int volunteerId){ //TODO keep? maybe not used at
     return false;
 }
 
-bool WareHouse::customerExists(int customerId){ //TODO keep? maybe not used at all
-    for (const auto& customers : customers) {
-        if(customers->getId() == customerId){
-            return true;
-        } 
-    }
-    return false;
+bool WareHouse::customerExists(int customerId){ 
+    return (customerId<WareHouse::customerCounter); //because not deleting customers
 }
 
-bool WareHouse::custCanMakeOrder(int customerId){ //checks if exists and can make order. for add order operation
+bool WareHouse::custCanMakeOrder(int customerId){ //checks if exists and can make order. for addOrder operation
     bool canMake = false;
     for (const auto& customers : customers) {
         if(customers->getId() == customerId && customers->canMakeOrder()){
@@ -137,8 +137,9 @@ bool WareHouse::orderExists(int orderId){ //TODO keep? maybe not used at all
 }
 
 //TODO getActions
-
-//
+const vector<BaseAction*> &WareHouse::getActions() const{
+    return WareHouse::actionsLog;
+}
 
 //TODO close
 
@@ -149,10 +150,9 @@ void WareHouse::open(){
     WareHouse::isOpen = true;
 }
 
-vector<Volunteer*> WareHouse::parseVolunteers(const string &configFilePath){
+void WareHouse::parseVolunteers(const string &configFilePath){
     std::fstream file(configFilePath, std::ios::in);
     vector<string> volunteersTemp; 
-    vector<Volunteer*> volunteersVector;
     std::string line;
     while (std::getline(file, line)) {
         if (line.find("volunteer") == 0) {
@@ -179,7 +179,7 @@ vector<Volunteer*> WareHouse::parseVolunteers(const string &configFilePath){
             if(volunteersTemp[i+1] == "collector"){
                 int coolDown = stoi(volunteersTemp[i+2]);
                 CollectorVolunteer *tempVol = new CollectorVolunteer(this->volunteerCounter, name, coolDown);
-                volunteersVector.push_back(tempVol);
+                volunteers.push_back(tempVol);
                 this->volunteerCounter++;
                 i += 2;
             }
@@ -187,7 +187,7 @@ vector<Volunteer*> WareHouse::parseVolunteers(const string &configFilePath){
                 int coolDown = stoi(volunteersTemp[i+2]);
                 int maxOrders = stoi(volunteersTemp[i+3]);
                 LimitedCollectorVolunteer *tempVol = new LimitedCollectorVolunteer(this->volunteerCounter, name, coolDown, maxOrders);
-                volunteersVector.push_back(tempVol);
+                volunteers.push_back(tempVol);
                 this->volunteerCounter++;
                 i += 3;
             }
@@ -195,7 +195,7 @@ vector<Volunteer*> WareHouse::parseVolunteers(const string &configFilePath){
                 int maxDistance = stoi(volunteersTemp[i+2]);
                 int distancePerStep = stoi(volunteersTemp[i+3]);
                 DriverVolunteer *tempVol = new DriverVolunteer(this->volunteerCounter, name, maxDistance, distancePerStep);
-                volunteersVector.push_back(tempVol);
+                volunteers.push_back(tempVol);
                 this->volunteerCounter++;
                 i += 3;
             }
@@ -204,24 +204,22 @@ vector<Volunteer*> WareHouse::parseVolunteers(const string &configFilePath){
                 int distancePerStep = stoi(volunteersTemp[i+3]);
                 int maxOrders = stoi(volunteersTemp[i+4]);
                 DriverVolunteer *tempVol = new DriverVolunteer(this->volunteerCounter, name, maxDistance, distancePerStep);
-                volunteersVector.push_back(tempVol);
+                volunteers.push_back(tempVol);
                 this->volunteerCounter++;
                 i += 4;
             }
         }
     }
 
-    // std::cout << "Volunteers vector:" << std::endl;
-    // for (const auto& volunteersVector : volunteersVector) {
-    //     std::cout << volunteersVector->toString() << std::endl; //delete later
-    // }
-    return volunteersVector;
+    std::cout << "Volunteers vector:" << std::endl;
+    for (const auto& volunteer : volunteers) {
+        std::cout << volunteer->toString() << std::endl; //delete later
+    }
 }
 
-vector<Customer*> WareHouse::parseCustomers(const string &configFilePath){
+void WareHouse::parseCustomers(const string &configFilePath){
     std::fstream file(configFilePath, std::ios::in);
     vector<string> customersTemp; 
-    vector<Customer*> customersVector;
     std::string line;
     while (std::getline(file, line)) {
         if (line.find("customer") == 0) {
@@ -249,7 +247,7 @@ vector<Customer*> WareHouse::parseCustomers(const string &configFilePath){
                 int distance = stoi(customersTemp[i+2]);
                 int maxOrders = stoi(customersTemp[i+3]);
                 SoldierCustomer *tempCust = new SoldierCustomer(this->customerCounter, name, distance, maxOrders);
-                customersVector.push_back(tempCust);
+                customers.push_back(tempCust);
                 this->customerCounter++;
                 i += 3;
             }
@@ -257,30 +255,26 @@ vector<Customer*> WareHouse::parseCustomers(const string &configFilePath){
                 int distance = stoi(customersTemp[i+2]);
                 int maxOrders = stoi(customersTemp[i+3]);
                 CivilianCustomer *tempCust = new CivilianCustomer(this->customerCounter, name, distance, maxOrders);
-                customersVector.push_back(tempCust);
+                customers.push_back(tempCust);
                 this->customerCounter++;
                 i += 3;
             }
         }
     }
     
-    // std::cout << "Customers vector:" << std::endl;
-    // for (const auto& customersVector : customersVector) {
-    //     std::cout << customersVector->toString() << std::endl; //delete later
-    // }
-    return customersVector;
+    std::cout << "Customers vector:" << std::endl;
+    for (const auto& customer : customers) {
+        std::cout << customer->toString() << std::endl; //delete later
+    }
 }
 
-// int main(){
-//     string fileLocation = "../bin/rest/configFileExample.txt";
-//     WareHouse wh(fileLocation);
-//     bool madeOrder = wh.newOrderbyID(2);
-//     cout << madeOrder << endl;
-//     Order &ord = wh.getOrder(1);
-//     cout << ord.toString() << endl;
-
-//     return 0;
-// }
+int main(){
+    string fileLocation = "../bin/rest/configFileExample.txt";
+    WareHouse wh(fileLocation);
+    // Customer *vol = &wh.getCustomer(1);
+    // cout << vol->toString() << endl;
+    return 0;
+}
 
 
 
