e1a1ef7ff5d1e079f3e27a404a3d6a49992aa066
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-01-28 13:14:28 +0200

    Update Action.h

--- Action.h
+++ Action.h
@@ -5,6 +5,7 @@
 using std::string;
 using std::vector;
 
+
 enum class ActionStatus{
     COMPLETED, ERROR
 };
@@ -57,7 +58,7 @@ class AddOrder : public BaseAction {
 
 class AddCustomer : public BaseAction {
     public:
-        AddCustomer(string customerName, string customerType, int distance, int maxOrders);
+        AddCustomer(const string &customerName, const string &customerType, int distance, int maxOrders);        
         void act(WareHouse &wareHouse) override;
         AddCustomer *clone() const override;
         string toString() const override;
@@ -138,3 +139,6 @@ class RestoreWareHouse : public BaseAction {
         string toString() const override;
     private:
 };
+
+
+
